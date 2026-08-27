ff85e3895c50995b345aadd6688b9c8dca7579a2
mohamadmahditavakoli <mohamadmahditavakoli8@gmail.com>
2026-08-27 16:31:54 +0330

    Add product detail

--- product_module/views.py
+++ product_module/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Product # . -> اشاره به اپ یا ماژولی که داخلش هستیم
+from django.shortcuts import render, get_object_or_404
+from .models import Product  # . -> اشاره به اپ یا ماژولی که داخلش هستیم
+from django.http import Http404
 
 
 def product_list(request):
@@ -7,3 +8,24 @@ def product_list(request):
     return render(request, 'product_module/product_list.html', {
         'products': products
     })
+
+
+def product_detail(request, product_id):
+    # روش اول مدیریت عدم وجود آیتم در جدول با برگرداندن صفحه 404:
+    # try:
+    #     product = Product.objects.get(id=product_id) # بجای id میتونیم از pk استفاده کنیم -> get(pk=id)
+    # except:
+    #     raise Http404
+
+    # روش بهتر و تمیز تر مدیریت عدم وجود آیتم در جدول با دستور get_object_or_404:
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'product_module/product_detail.html', {
+        'product': product
+    })
+
+# -------------------------------------------------------------------------------------------------------
+# get_object_or_404(طرز فیلتر شدن آیتم ها براساس فیلدی مثل آیدی, کلاس مدل):
+
+# این دستور یا آبجکت رو بدست میاورد یا درصورت وجود نداشتن آن، صفحه 404 را برمیگرداند و کد تمیز تری هست
+
+# -------------------------------------------------------------------------------------------------------
