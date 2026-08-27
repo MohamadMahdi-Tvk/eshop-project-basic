3ae4edc69bb9dde9c93ac685b47c2eebf5ed4325
mohamadmahditavakoli <mohamadmahditavakoli8@gmail.com>
2026-08-27 12:52:41 +0330

    Add product list

--- product_module/views.py
+++ product_module/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product # . -> اشاره به اپ یا ماژولی که داخلش هستیم
 
-# Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_module/product_list.html', {
+        'products': products
+    })
